85cb3ef3455cfe091cf03c4b6763799da3720312
Antgst <ant.gousset@gmail.com>
2025-10-20 13:22:02 +0000

    add

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("sizeof(char)=%zu\n", sizeof(char));
+	printf("sizeof(char)=%zu\n", sizeof(short));
+	printf("sizeof(char)=%zu\n", sizeof(int));
+	printf("sizeof(char)=%zu\n", sizeof(long));
+	return (0);
+}
