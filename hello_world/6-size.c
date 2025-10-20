de9049beb1ecd830084c78521c1dc4eb44c239a2
Antgst <ant.gousset@gmail.com>
2025-10-20 13:31:49 +0000

    add

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,9 +9,9 @@
 int main(void)
 {
 	printf("Size of a char: =%zubyte(s) \n", sizeof(char));
-	printf("Size of a char: =%zubyte(s) \n", sizeof(int));
-	printf("Size of a char: =%zubyte(s) \n", sizeof(long int));
-	printf("Size of a char: =%zubyte(s) \n", sizeof(long long int));
-	printf("Size of a char: =%zubyte(s) \n", sizeof(float));
+	printf("Size of an int: =%zubyte(s) \n", sizeof(int));
+	printf("Size of a long int: =%zubyte(s) \n", sizeof(long int));
+	printf("Size of a long long int: =%zubyte(s) \n", sizeof(long long int));
+	printf("Size of a float: =%zubyte(s) \n", sizeof(float));
 	return (0);
 }
