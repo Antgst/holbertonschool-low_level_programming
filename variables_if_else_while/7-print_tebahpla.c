48b19abb9483dd7bc892c5af2762392a5d492b28
Antgst <ant.gousset@gmail.com>
2025-10-21 14:00:58 +0000

    add

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**                                                                                                                                                                                                            
- * main - Entry point                                                                                                                                                                                          
- *                                                                                                                                                                                                             
- * Return: Always 0 (Success)                                                                                                                                                                                  
+/**                                                                                                                                                                                                           
+ * main - Entry point                                                                                                                                                                                         
+ *                                                                                                                                                                                                             * Return: Always 0 (Success)                                                                                                                                                                               
  */
 
 int main(void)
 {
   char i;
-  char m;
 
   i = 'z';
   while (i >= 'a')
