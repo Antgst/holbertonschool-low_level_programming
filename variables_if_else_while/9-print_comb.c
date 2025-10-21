5c970b6ef8fbaaa444601f30985ea957ec1b27de
Antgst <ant.gousset@gmail.com>
2025-10-21 15:29:27 +0000

    add

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -5,18 +5,19 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int i;
 
-	for (int i = 0; i <= 9; i++)
-{
-	if (i) { putchar(','); putchar(' '); }
-	putchar('0' + i);
-}
-putchar('\n');
-
- return (0);
-
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
