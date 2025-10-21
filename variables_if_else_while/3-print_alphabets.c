88ec596639b55430ae36ceb23867bc78f99adada
Antgst <ant.gousset@gmail.com>
2025-10-21 12:49:27 +0000

    add

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  char i = 'a';
+  char I = 'A';
+  while (i <= 'a')
+    {
+      putchar (i);
+      i++;
+    }
+  while (I <= 'Z');
+  {
+    putchar (I);
+    I++;
+   }
+  return (0);
+}
