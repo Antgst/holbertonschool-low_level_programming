54e6ac71995ee9a67314e3719ce50f47b3efc591
Antgst <ant.gousset@gmail.com>
2025-10-21 14:14:38 +0000

    add

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  char i;
+
+  for (i = 30; i < 39; i++)
+    {
+      putchar(i);
+    }
+  putchar('\n');
+  return (0);
+}
