bf63d2b11fb39b7e25e5576c8f9199e46086b28d
Antgst <ant.gousset@gmail.com>
2025-10-21 14:23:04 +0000

    add

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -5,15 +5,18 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-  char i;
+	char c;
 
-  for (i = 30; i < 40; i++)
-    {
-      putchar(i);
-    }
-  putchar('\n');
-  return (0);
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
