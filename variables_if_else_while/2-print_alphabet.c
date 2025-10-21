eb0a4d9c2042113bcbe138ee1d0b108b77139041
Antgst <ant.gousset@gmail.com>
2025-10-21 12:22:39 +0000

    add

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,11 @@
  * Return: Always 0 (Success)
  */
 
-int main(void) 
+int main(void)
 {
-  char alphabet;
-  for (alphabet = 'a'; alphabet <= "z"; alphabet++)
-    putchar(alphabet);
-  putchar('\n');
+  char i = 'a'
+
+    while (i <= 'z')
+      putchar (i++);
   return (0);
 }
