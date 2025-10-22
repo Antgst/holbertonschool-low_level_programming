3ee7f8aec3730eeb849c017318979b12d74ffad8
Antgst <ant.gousset@gmail.com>
2025-10-22 12:45:08 +0000

    add

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int i)
+
+{
+  if (i < '0')
+  {
+    return (-i);
+  }
+
+  if (i > '0')
+  {
+    return (i);
+  }
+
+}
