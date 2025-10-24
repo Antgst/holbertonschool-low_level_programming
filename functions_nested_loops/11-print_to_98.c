37e0aa5ff1d4fa71a480afcfdad1f7d9e1d05040
Antgst <ant.gousset@gmail.com>
2025-10-24 07:59:25 +0000

    add

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_to_98(int n)
+
+{
+  
+  if (n <= 98)
+
+  {
+    for (; n <= 98; n++)
+
+    {
+      printf("%d", n);
+
+      if (n != 98)
+      putchar(',');
+      putchar(' ');
+
+    }
+
+  }
+
+}
