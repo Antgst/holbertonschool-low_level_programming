e9f88026c122e777c27e5b716dc294dd07a856ee
Antgst <ant.gousset@gmail.com>
2025-10-24 10:18:02 +0000

    add

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -9,13 +9,13 @@
 
 int main(void)
 {
-  char i = 'a';
+	char i = 'a';
 
-    while (i <= 'z')
-      {
-      putchar (i);
-      i++;
-      }
-      putchar ('\n');
-  return (0);
+	while (i <= 'z')
+	{
+		putchar (i);
+		i++;
+	}
+	putchar ('\n');
+	return (0);
 }
