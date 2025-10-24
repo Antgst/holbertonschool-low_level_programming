8729b75013f61eedf867bceb6820f9414e6279ef
Antgst <ant.gousset@gmail.com>
2025-10-24 10:39:18 +0000

    add

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -9,12 +9,12 @@
 
 int main(void)
 {
-  char i;
-  
-    for (i = 48; i < 58; i++)
-      {
-	putchar(i);
-      }
-  putchar('\n');
-  return (0);
+	char i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
