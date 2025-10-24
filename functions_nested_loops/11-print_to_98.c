e26f90376248564b61be11efc739934373e0ec0e
Antgst <ant.gousset@gmail.com>
2025-10-24 09:04:42 +0000

    add

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,47 +8,23 @@
  */
 
 void print_to_98(int n)
-
 {
-  if (n <= 98)
-
-    {
-
-      for (; n <= 98; n++)
-
-	{
-
-	  printf("%d", n);
-
-	}
-
-	  if (n != 98)
-
-	  putchar(',');
-	  putchar(' ');
-	  putchar('\n');
-	  
+    if (n <= 98) {
+        for (; n <= 98; n++) {
+            printf("%d", n);
+            if (n != 98) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    } else {
+        for (; n >= 98; n--) {
+            printf("%d", n);
+            if (n != 98) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
     }
-
-  else
-
-     {
-
-       for (; n >= 98; n--)
-
-	 {
-
-	   printf("%d", n);
-
-	 }
-
-	   if (n != 98)
-
-	   putchar(',');
-	   putchar(' ');
-	   putchar('\n');
-
-     }
-
+    putchar('\n');
 }
-
