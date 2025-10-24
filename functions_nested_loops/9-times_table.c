7e08982bcc2d95799208ffe1f1fa9c79c7df3ffd
Antgst <ant.gousset@gmail.com>
2025-10-24 11:03:57 +0000

    add

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,44 +7,29 @@
  */
 
 void times_table(void)
-
 {
+	int a, b, resultat;
 
-  int a, b, resultat;
- 
-  for (a = 0; a <= 9; a++)
-
-    {
-      for (b = 0; b <= 9; b++)
-
+	for (a = 0; a <= 9; a++)
 	{
-	  resultat = a * b;
-
-	    if (b != 0)
-       
-	      {
-		_putchar(',');
-		_putchar(' ');
-
-		if (resultat < 10)
-
-		  _putchar(' ');
-	      }
-
-	    if (resultat < 10)
-
-	      _putchar(resultat + '0');
-
-	    else
-	      {
-		_putchar((resultat / 10) + '0');
-		_putchar((resultat % 10) + '0');
-	      }
-	   
-	    
+		for (b = 0; b <= 9; b++)
+		{
+			resultat = a * b;
+			if (b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (resultat < 10)
+					_putchar(' ');
+			}
+			if (resultat < 10)
+				_putchar(resultat + '0');
+			else
+			{
+				_putchar((resultat / 10) + '0');
+				_putchar((resultat % 10) + '0');
+			}
+		}
+		_putchar('\n');
 	}
-      _putchar('\n');
-    }
-
-	   
 }
