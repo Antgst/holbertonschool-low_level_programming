29bebd50846226421271a3cb7f45736d75165dab
Antgst <ant.gousset@gmail.com>
2025-10-24 11:00:53 +0000

    add

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,20 +7,19 @@
  */
 
 void jack_bauer(void)
-
 {
-  int minutes, hours;
+	int minutes, hours;
 
-  for (hours = 0; hours <= 23; hours++)
-  {
-    for (minutes = 0; minutes <= 59; minutes++)
-    {
-      _putchar((hours / 10) + '0');
-      _putchar((hours % 10) + '0');
-      _putchar(':');
-      _putchar((minutes / 10) + '0');
-      _putchar((minutes % 10) + '0');
-      _putchar('\n');
-    }
-  }
+	for (hours = 0; hours <= 23; hours++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
