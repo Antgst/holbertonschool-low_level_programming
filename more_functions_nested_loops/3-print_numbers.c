7f7cae3c763cc57571981700548b7b4125f03269
Antgst <ant.gousset@gmail.com>
2025-10-27 10:02:53 +0000

    add

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - Imprime les nombres
+ *
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+	char n;
+{
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
