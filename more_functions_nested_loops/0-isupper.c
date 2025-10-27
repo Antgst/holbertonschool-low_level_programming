5bcf7dfdc08a28fcfd0c8b310f2593c7c40c87ef
Antgst <ant.gousset@gmail.com>
2025-10-27 09:02:55 +0000

    add

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'a' && c<= 'z')
+	{
+		return (0);
+	}
+	return (1);
+}
