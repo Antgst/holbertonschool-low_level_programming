46c6c26efd424c38418468d86620e70e2f12d9e3
Antgst <ant.gousset@gmail.com>
2025-10-27 09:32:43 +0000

    add

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - verifie si nombre
+ * @c - nombre
+ * Return: Always 0.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
