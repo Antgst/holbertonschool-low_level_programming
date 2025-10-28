78a160745ba19916c2a5046090ea18b251b84805
Antgst <ant.gousset@gmail.com>
2025-10-28 09:26:37 +0000

    add

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main (void)
+{
+	int nombre;
+
+	{
+		for (nombre = 1; nombre <= 100; nombre++)
+		{
+			if (nombre % 3 == 0 && nombre % 5 == 0)
+			printf("FizzBuzz");
+
+			else if (nombre % 3 == 0)
+			printf("Fizz");
+
+			else if (nombre % 5 == 0)
+			printf("Buzz");
+
+			else
+				printf("%d", nombre);
+			if (nombre < 100)
+				printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
