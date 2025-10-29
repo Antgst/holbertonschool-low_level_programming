20fbc9e90b4736de52cd1db302f4d1b802455f37
Antgst <ant.gousset@gmail.com>
2025-10-29 10:15:25 +0000

    add

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (len > 0)
+	{
+		len--;
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
