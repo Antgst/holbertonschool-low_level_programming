8d04ef0307c226e6ec86862e545d8ef045c224fb
Antgst <ant.gousset@gmail.com>
2025-10-29 12:41:37 +0000

    add

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -7,7 +7,6 @@
  * Return: Always 0.
  */
 
-/**
 int _strlen(char *s)
 {
 	int len = 0;
@@ -17,13 +16,4 @@ int _strlen(char *s)
 		len++;
 	}
 	return (len);
-}
-
-*/
-
-size_t _strlen_ptr(const char *s)
-{
-    const char *p = s;
-    while (*p) p++;
-    return (size_t)(p - s);
 }
