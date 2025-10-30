a56d7ab91214b7b0155f6cb2e341375059fe9a42
Antgst <ant.gousset@gmail.com>
2025-10-30 18:13:09 +0100

    Update 0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * reset_to_98 - updates the value it points.
+ * @n: pointer
+ * Return: 0.
  */
 
 void reset_to_98(int *n)
