e1bd1f5c93d972b4b2a73c5cd8b6a62a176e6f38
Antgst <ant.gousset@gmail.com>
2025-10-30 18:15:52 +0100

    Update 3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,9 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _puts - updates the value it points.
+ * @str: string
+ * Return: 0.
  */
 
 void _puts(char *str)
