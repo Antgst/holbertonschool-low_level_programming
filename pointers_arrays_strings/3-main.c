b161b18a173b208e7bdcde97c900aea7baec2580
Antgst <ant.gousset@gmail.com>
2025-10-31 12:34:11 +0000

    add

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -1,10 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - check the code
  *
  * Return: Always 0.
- */
+ 
 int main(void)
 {
     char *str;
@@ -12,7 +13,8 @@ int main(void)
     str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
     _puts(str);
     return (0);
-}
+    }
+ */
 
 #include "main.h"
 #include <stdio.h>
