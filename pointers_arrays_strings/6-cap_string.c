6c1447b07efbe381fe8ce5f44cbbaf74ce107a45
Antgst <ant.gousset@gmail.com>
2025-10-31 15:22:57 +0000

    add

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * cap_string - Change all firsts lettersof a word of a string to uppercase
+ * @str: pointer to the input string
+ *
+ * Return: pointer to the modified string
+**/
+
+char *cap_string(char *str)
+{
+	int i;
+	int j;
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= ('a' - 'A');
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (str[i] == sep[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+					str[i + 1] -= ('a' - 'A');
+				break;
+			}
+		}
+	}
+	return str;
+}
