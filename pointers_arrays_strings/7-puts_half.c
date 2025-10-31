b048a71f2ed623124ca185c0fd78b30afadf6761
Antgst <ant.gousset@gmail.com>
2025-10-31 18:14:08 +0000

    add

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,9 @@
-#include "main.h"
+/**#include "main.h"
 
-/**
  * _strlen - check the len of an str.
  * @s: char *
  * Return: the len.
- */
+
 
 void puts_half(char *str)
 {
@@ -20,5 +19,36 @@ void puts_half(char *str)
 		start = len - ((len + 1) / 2);
 	for (i = start; i < len; i++)
 		_putchar(str[i]);
+	_putchar('\n');
+}
+*/
+
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string, then a newline
+ * @str: pointer to the input string
+ *
+ * Description: If the length is odd, prints the last (length + 1) / 2 chars.
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int start;
+	int i;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		start = len / 2;
+	else
+		start = len - ((len + 1) / 2);
+
+	for (i = start; i < len; i++)
+		_putchar(str[i]);
+
 	_putchar('\n');
 }
