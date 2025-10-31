a2e7ebd5bb82a2cc09185c2a94d00f90b15b05bc
Antgst <ant.gousset@gmail.com>
2025-10-31 10:28:48 +0000

    add

--- pointers_arrays_strings/2-main.c
+++ pointers_arrays_strings/2-main.c
@@ -5,7 +5,7 @@
  * main - check the code
  *
  * Return: Always 0.
- */
+
 int main(void)
 {
     char *str;
@@ -20,7 +20,6 @@ int main(void)
 #include "main.h"
 #include <stdio.h>
 
-/**
  * main - check the code
  *
  * Return: Always 0.
---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (src[i] != '\0')
+			dest[i] = src[i];
+		else
+			dest[i] = '\0';
+	}
+	return (dest);
+}
