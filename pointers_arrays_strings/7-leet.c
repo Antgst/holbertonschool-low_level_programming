badd9001f48c55ab1daeb4753d1bdcc1b47b3deb
Antgst <ant.gousset@gmail.com>
2025-10-31 15:27:00 +0000

    add

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337
+ * @str: pointer to the input string
+ *
+ * Return: pointer to the modified string
+**/
+
+char *leet(char *str)
+{
+	int i;
+	int j;
+	char from[] = "aAeEoOtTlL";
+	char to[]   = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; from[j] != '\0'; j++)
+		{
+			if (str[i] == from[j])
+			{
+				str[i] = to[j];
+				break;
+			}
+		}
+	}
+	return str;
+}
