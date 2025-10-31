2d7482e59e754f1d0bdc1d286d107beec7ee10cb
Antgst <ant.gousset@gmail.com>
2025-10-31 19:08:40 +0000

    add

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,27 +2,39 @@
 #include <stdio.h>
 
 /**
-**/
+ * _atoi - converts a string to an integer
+ * @s: pointer to the input string
+ *
+ * Description: Counts all '+' and '-' seen before the first digit to set
+ * the sign. Accumulates consecutive digits, then stops at the first
+ * non-digit after starting. If no digit exists, returns 0.
+ *
+ * Return: the converted integer
+ */
 int _atoi(char *s)
 {
 	int nb = 0;
 	int element;
 	int sign = 1;
+	int started = 0;
 
 	for (element = 0; s[element] != '\0'; element++)
 	{
-		if (s[element] == '-')
+		if (!started && (s[element] == '-' || s[element] == '+'))
 		{
-			sign *= -1;
+			if (s[element] == '-')
+				sign *= -1;
 		}
 		else if (s[element] >= '0' && s[element] <= '9')
 		{
+			started = 1;
 			nb = nb * 10 + (s[element] - '0');
 		}
-		else
+		else if (started)
 		{
-			;
+			break;
 		}
 	}
+
 	return (nb * sign);
 }
