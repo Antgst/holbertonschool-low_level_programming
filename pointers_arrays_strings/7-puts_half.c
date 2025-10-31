d078e20e51c31b46ed775be303fc92fa68441e16
Antgst <ant.gousset@gmail.com>
2025-10-31 18:54:22 +0000

    add

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,29 +1,3 @@
-/**
-#include "main.h"
-
- * _strlen - check the len of an str.
- * @s: char *
- * Return: the len.
-
-
-void puts_half(char *str)
-{
-	int len = 0;
-	int start;
-	int i;
-
-	while (str[len] != '\0')
-		len++;
-	if (len % 2 == 0)
-		start = len / 2;
-	else
-		start = len - ((len + 1) / 2);
-	for (i = start; i < len; i++)
-		_putchar(str[i]);
-	_putchar('\n');
-}
-*/
-
 #include "main.h"
 
 /**
