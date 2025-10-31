51a902744a54159d76ed2fe4140a9e0035deccd6
Antgst <ant.gousset@gmail.com>
2025-10-31 19:19:08 +0000

    add

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,58 +1,36 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _atoi - converts a string to an integer
- * @s: pointer to the input string
+ * _atoi - returns the length of a string
  *
- * Description: Compte tous les signes '+' et '-' avant le premier chiffre
- * pour déterminer le signe. Accumule uniquement les chiffres consécutifs,
- * s'arrête au premier non-chiffre après le début. Évite l'overflow signé
- * sans <limits.h> en bornant contre INT_MAX/INT_MIN dérivés.
+ * @s: string to analyse
  *
- * Return: the converted integer, or 0 if no digit is found
+ * Return: returns lenght;
  */
 
 int _atoi(char *s)
 {
-	int nb = 0;
-	int element;
-	int sign = 1;
-	int started = 0;
-	unsigned int umax = ~0U;
-	int imax = (int)(umax >> 1);
-	int imin = -imax - 1;
+	short boolean;
+	int i, minus, result;
 
-	for (element = 0; s[element] != '\0'; element++)
+	i = minus = result = boolean = 0;
+	minus = -1;
+
+	while (s[i] != '\0')
 	{
-		if (!started && (s[element] == '-' || s[element] == '+'))
-		{
-			if (s[element] == '-')
-				sign = -sign;
-		}
-		else if (s[element] >= '0' && s[element] <= '9')
-		{
-			int d = s[element] - '0';
+		if (s[i] == '-')
+			minus *= -1;
 
-			started = 1;
-
-			if (sign == 1)
-			{
-				if (nb > (imax - d) / 10)
-					return (imax);
-				nb = nb * 10 + d;
-			}
-			else
-			{
-				if (nb < (imin + d) / 10)
-					return (imin);
-				nb = nb * 10 - d;
-			}
-		}
-		else if (started)
+		if (s[i] >= '0' && s[i] <= '9')
 		{
+			result *= 10;
+			result -= (s[i] - '0');
+			boolean = 1;
+		}
+		else if (boolean == 1)
 			break;
-		}
+		i++;
 	}
-	return (nb);
+	result *= minus;
+	return (result);
 }
