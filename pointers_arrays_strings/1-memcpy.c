7add2de9648fd857878dca0b7ffb551b3b1e22a1
Antgst <ant.gousset@gmail.com>
2025-11-03 13:17:51 +0000

    add

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination
+ * @src: source
+ * @n: length max
+ * Return: the copy
+**/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for ( i = 0; i < n && src [i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+			dest[i] = '\0';
+	}
+	return (dest);
+}
