bd973ac5b5f36df3375587b7da84e8738700fd32
Antgst <ant.gousset@gmail.com>
2025-11-05 07:46:49 +0000

    add

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -12,13 +12,13 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for ( i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
 	for (; i < n; i++)
 	{
-			dest[i] = '\0';
+		dest[i] = '\0';
 	}
 	return (dest);
 }
