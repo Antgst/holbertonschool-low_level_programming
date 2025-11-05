a5656b5451d445fd719461115c4e121358657d89
Antgst <ant.gousset@gmail.com>
2025-11-05 07:35:31 +0000

    add

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -10,7 +10,15 @@
  *
  * Return: The result of x raised to the power of y, or -1 if y < 0
  */
-
 int _pow_recursion(int x, int y)
 {
-	
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -7,7 +7,27 @@
  *
  * Return: the value of y.
 **/
-
 int search_y(int n, int y)
 {
-	
+	if (y * y == n)
+	{
+		return (y);
+	}
+	if (y * y > n)
+	{
+		return (-1);
+	}
+	return (search_y(n, y + 1));
+}
+
+
+/**
+ * _sqrt_recursion - returns natural square root of n
+ * @n: number to find the root of
+ *
+ * Return: square root or -1 if none
+**/
+int _sqrt_recursion(int n)
+{
+	return (search_y(n, 0));
+}
