c455cae5bd08b80b9d7dc9983daca42ec351fa55
Antgst <ant.gousset@gmail.com>
2025-11-07 16:15:32 +0000

    add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 /**
- * main - Adds positive numbers passed as arguments
- * @argc: Number of arguments
- * @argv: Array of argument strings
+ * main - get the name of the program
  *
- * Return: 0 if success, 1 if non-digit is found
-**/
+ * @argc: number of argument
+ * @argv: array of argument
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int somme = 0;
+
+	if (argc == 1)
+	{
+		;
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!(isdigit(argv[i][j])))
+			{
+			printf("Error\n");
+			return (1);
+			}
+		}
+
+	somme += atoi(argv[i]);
+	}
+	printf("%d\n", somme);
+	return (0);
+}
