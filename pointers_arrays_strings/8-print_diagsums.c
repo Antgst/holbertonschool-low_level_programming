9861885cbcecab34158e7798499197dc0507bf94
Antgst <ant.gousset@gmail.com>
2025-11-07 13:11:27 +0000

    ad

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -14,10 +14,14 @@ void print_diagsums(int *a, int size)
 	int sum1;
 	int sum2;
 
-		for (i = 0; i < size; i++)
-		{
-			sum1 += (a[i * size + i]);
-			sum2 += (a[i * size + (size - 1 - size - i)]);
-		}
-		printf("%d %d", sum1, sum2);
+	sum1 = 0;
+	sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += (a[i * size + i]);
+		sum2 += (a[i * size + (size - 1 - size - i)]);
+	}
+	printf("%d %d", sum1, sum2);
+	printf("\n");
 }
