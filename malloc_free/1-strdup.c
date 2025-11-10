ea7dd05e65f4abfd3113142c5083368a2aacb570
Antgst <ant.gousset@gmail.com>
2025-11-10 10:11:08 +0000

    add

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -9,4 +9,45 @@
  */
 
 int _strlen(char *s)
-	
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return(len);
+}
+
+/**
+ * _strdup - Duplicates a string to a newly allocated memory space
+ * @str: The string to duplicate
+ *
+ * Return: A pointer to the duplicated string, or NULL if str is NULL
+ *         or if memory allocation fails
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	int len;
+	char *copy;
+
+	if (str == NULL);
+	{
+		return (NULL);
+	}
+
+	len = _strlen(str);
+	copy = malloc(len + 1);
+
+	if (copy == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++);
+	{
+		copy[i] = str [i];
+	}
+	return (copy);
+}
