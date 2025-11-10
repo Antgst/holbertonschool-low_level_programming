5e8b3526f91599db1ac54c57d0aa1ac71ec89f4f
Antgst <ant.gousset@gmail.com>
2025-11-10 09:32:42 +0000

    add

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,3 +13,21 @@
  */
 
 char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *p = malloc(sizeof(c) * size);
+
+	if (p == 0)
+	{
+		return (NULL);
+	}
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	return (p);
+}
