01504fbf679cf6f595cd99a733c80524bfa32f2b
Antgst <ant.gousset@gmail.com>
2025-11-12 09:50:00 +0000

    add

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -21,14 +21,17 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
+
 	p = malloc(nmemb * size);
 	if (p == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < nmemb; i++)
+
+	for (i = 0; i < nmemb * size; i++)
 	{
 		p[i] = 0;
 	}
+
 	return ((void *)p);
 }
