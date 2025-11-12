d6b111974090d9a1d9685bc46009e1db80ba55d1
Antgst <ant.gousset@gmail.com>
2025-11-12 09:45:31 +0000

    add

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -7,6 +7,28 @@
  *
  * Return: Pointer to the newly allocated array, or NULL on failure.
  */
-
 int *array_range(int min, int max)
 {
+	int i, len;
+	int *p;
+
+	len = max - min + 1;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	p = malloc(len * sizeof(int));
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		p[i] = min + i;
+	}
+	return (p);
+}
