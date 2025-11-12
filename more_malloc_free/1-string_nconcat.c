5af97539ae8dc41fb463fc01a165a423df5bc8ad
Antgst <ant.gousset@gmail.com>
2025-11-12 08:21:46 +0000

    add

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+/**
+ * _strlen - check the len of an str.
+ * @s: char *
+ * Return: the len.
+ */
+
+int _strlen(char *s)
+{
---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * array_range - Creates an array of integers from min to max.
+ * @min: Minimum integer (inclusive).
+ * @max: Maximum integer (inclusive).
+ *
+ * Return: Pointer to the newly allocated array, or NULL on failure.
+ */
+
+int *array_range(int min, int max)
+{
