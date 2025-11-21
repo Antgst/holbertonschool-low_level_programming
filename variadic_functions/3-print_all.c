7f3bffa72645a306da6cd9934b49a116ab480977
Antgst <ant.gousset@gmail.com>
2025-11-21 13:10:11 +0100

    add

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,53 +1,97 @@
-#include <stdarg.h>
 #include "variadic_functions.h"
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
- * print_all: Print all the operations
- * @format: list of types of arguments passed to the function
- * @...: variable number of arguments to be printed
- * 
- * Return: nothing 
- */
+ * print_char - Print a charactere
+ * @list: the list of argument
+ * Return: void
+**/
+void print_char(va_list *list)
+{
+	printf("%c", va_arg(*list, int));
+}
 
+/**
+ * print_int - Print an integer
+ * @list: the list of argument
+ * Return: void
+**/
+void print_int(va_list *list)
+{
+	printf("%d", va_arg(*list, int));
+}
+
+/**
+ * print_float - Print a flaot
+ * @list: the list of argument
+ * Return: void
+**/
+void print_float(va_list *list)
+{
+	printf("%f", va_arg(*list, double));
+}
+
+/**
+ * print_string - Print a string
+ * @list: the list of argument
+ * Description: Check directly if the string is NULL and print (nil)
+ * Return: void
+**/
+void print_string(va_list *list)
+{
+	char *s;
+
+	s = va_arg(*list, char *);
+
+	if (s == NULL)
+	{
+		printf("(nil)");
+	}
+	printf("%s", s);
+}
+
+/**
+ * print_all - prints anythings
+ * @format: a list of types of arguments
+ * Return: void.
+**/
 void print_all(const char * const format, ...)
- {
-    va_list ap;
-    unsigned int i = 0;
-    char *str;
-    char *sep = "";
+{
+	type_t type_print[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{0, NULL}
+	};
 
-    va_start (ap, format);
-    
-    while (format && format[i])
-    {
+	va_list argument_list;
 
+	unsigned int format_index, types_index;
+	char *separator;
 
+	separator = "";
+	format_index = 0;
+	va_start(argument_list, format);
 
+	while (format != NULL && format[format_index] != '\0')
+	{
+		types_index = 0;
+		while (type_print[types_index].types != 0)
+		{
+			if (format[format_index] == type_print[types_index].types)
+			{
+				printf("%s", separator);
+				type_print[types_index].print_func(&argument_list);
+				separator = ", ";
+				break;
+			}
+			types_index++;
+		}
+		format_index++;
+	}
 
-
-
-
-        
-        i++;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    va_end (ap);
-
-
- }
+	va_end(argument_list);
+	printf("\n");
+}
