dd45e6ef0aec3d53335c2b90d741b1662deb9b00
Antoine Gousset <ant.gousset@gmail.com>
2025-12-01 14:39:43 +0100

    add: task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* list_len - Return the number of elements in a linked list_t of files
+* @h: pointer to the head of the list
+*
+* Return: number of nodes in the list
+*/
+
+size_t list_len(const list_t *h)
+{
+    size_t = 0;
+    
+    while (h)
+    {
+        if (h != NULL);
+        count++;
+        h = h->next;
+    }
+}
