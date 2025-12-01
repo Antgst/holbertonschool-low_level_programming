4d69a91f7b9e01ae1bd92e004b4fc71ea58d4413
Antoine Gousset <ant.gousset@gmail.com>
2025-12-01 14:19:54 +0100

    add: function for the task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* print_list - function that print a list of elements
+* @h: pointer to the first node
+*
+* Return: the number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL) //ou while (h)
+	{
+		if (h->str) //ou if (h->str != NULL)
+			printf("[%u] %s\n", h->len, h->str);
+
+		else
+			printf("[0] (nil)\n");
+			
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
