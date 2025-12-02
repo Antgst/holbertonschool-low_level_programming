ddd3330fbc31104a80f7d4bb628b07e32f3d64b6
Antgst <ant.gousset@gmail.com>
2025-12-02 14:20:38 +0100

    fix: edit the task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,9 +13,9 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+		unsigned int len = 0;
 
 	new_node = malloc(sizeof(list_t));
-	unsigned int len = 0;
 
 	if (new_node == NULL)
 	{
