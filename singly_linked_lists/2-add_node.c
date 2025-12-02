36b2447fc161144b121dc02ae6ba632bdce40bee
Antgst <ant.gousset@gmail.com>
2025-12-02 10:22:00 +0100

    fix: remove whitespaces and blanks

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -25,7 +25,7 @@ list_t *add_node(list_t **head, const char *str)
 	 * else apparement optionnel
 	 */
 	{
-		new_node->str = strdup (str);
+		new_node->str = strdup(str);
 
 		if (new_node->str == NULL)
 		{
