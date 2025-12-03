5ffb42ce21bf397c77840e7b737ae81c64a784f4
Antgst <ant.gousset@gmail.com>
2025-12-03 13:56:49 +0100

    fix: fix spaces and betty task 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -4,13 +4,13 @@
 /**
  * add_dnodeint - Add a new node at the beginning of a dlistint_t list
  * @head: pointer to pointer to the head of the list
- * @n value to store inside the new node
+ * @n: value to store inside the new node
  *
  * Return: the adress of the new element, or NULL if it failed
  */
 
- dlistint_t *add_dnodeint(dlistint_t **head, const int n)
- {
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
 	dlistint_t *new;
 
 	if (head == NULL)
@@ -26,8 +26,8 @@
 	new->next = *head;
 
 	if (*head != NULL)
-	*head->prev = new
-	*head = new;
+	(*head)->prev = new;
+	(*head) = new;
 
 	return (new);
 
