18d05ae1b63624c7f430918d88647a1febe113c5
Antgst <ant.gousset@gmail.com>
2025-12-04 20:30:10 +0100

    fix: adjustments to task 2

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -19,11 +19,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 
 	if (!filename)
+	{
 	return (0);
+	}
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
 	return (0);
+	}
 
 	buffer = malloc(sizeof(char) * letters);
 
@@ -45,7 +49,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(buffer);
 
 	if (write2 != read2)
-	return (0);
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
 	return (write2);
 }
