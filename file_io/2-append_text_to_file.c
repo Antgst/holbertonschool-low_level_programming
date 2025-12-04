846169db86411ae52095695136e032e5b8c485ea
Antgst <ant.gousset@gmail.com>
2025-12-04 22:39:49 +0100

    add: add task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -11,7 +11,37 @@
  * Return: Success = 1, Fail = -1
  */
 
-int append_text_to_file(const char *filename, char *text_content)
+ int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd;
+	ssize_t write2;
+	size_t len = 0;
 
+	if (filename == NULL)
+	return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+		len++;
+	}
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+	return (-1);
+
+	if (len == 0)
+	{
+		close(fd);
+		return(-1);
+	}
+	
+	write2 = write(fd, text_content, len);
+	if (write2 == -1 || (size_t)write2 != len)
+	{
+		close(fd);
+		return(-1);
+	}
+	close(fd);
+	return(1);
 }
