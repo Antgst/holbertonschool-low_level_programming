3a55f23f8855e7f900fbae0eedab6ade9770d1dd
Antgst <ant.gousset@gmail.com>
2025-12-04 22:13:30 +0100

    add: task 1 of P3

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,5 +13,32 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	int fd;
+	ssize_t write2;
+	size_t textlen = 0;
 
+	if (filename == NULL)
+	return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[textlen] != '\0')
+		textlen++;
+	}
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+	return (-1);
+
+	if (textlen > 0)
+	{
+		write2 = write(fd, text_content, textlen);
+		if (write2 == -1 || (size_t)write2 != textlen)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
 }
