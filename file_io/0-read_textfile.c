1ed012dc5cb945164bff985a86d158786836c6ce
Antgst <ant.gousset@gmail.com>
2025-12-04 21:10:50 +0100

    fix: betty maximum lines

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -14,44 +14,36 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t read2;
-	ssize_t write2;
+	ssize_t read2, write2;
 	char *buffer;
 
-	if (!filename)
-	{
+	if (filename == NULL)
 	return (0);
-	}
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
 	return (0);
-	}
 
 	buffer = malloc(sizeof(char) * letters);
-
-	if (!buffer)
+	if (buffer == NULL)
 	{
 		close(fd);
 		return (0);
 	}
 
 	read2 = read(fd, buffer, letters);
-	close(fd);
 	if (read2 == -1)
 	{
 		free(buffer);
+		clode(fd);
 		return (0);
 	}
 
 	write2 = write(STDOUT_FILENO, buffer, read2);
 	free(buffer);
-
+	close(fd);
 	if (write2 != read2)
-	{
 		return (0);
-	}
 
 	return (write2);
 }
